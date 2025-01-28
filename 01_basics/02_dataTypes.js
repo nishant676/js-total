3821d690182eef2489ad012412957015953da0bb
Nishant Mishra <88461593+nishant676@users.noreply.github.com>
2025-01-28 18:55:02 +0000

    DataTypes and how to use

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // reat all JS code as latest vs.
+
+//alert(3+4) // we are using nodejs , not browser
+
+let name = "Nishant"
+let age = 23
+let isLoggedIn = true
+
+
+/*
+number ---> 2 to power 53
+bigint
+String ----> "" double coat or single
+boolean ----> true/false
+null   -----> standAlone value
+undefined ----> till now value not assigned
+symbol -----> unique ness 
+*/
+
+
+//object 
+
+console.log(typeof age+"-->Age type")//number
+console.log(typeof name+"---> name type")//string
+console.log(typeof undefined+"---> undefined type")//undefined
+console.log(typeof null+"---> null type")//object
+
+
+

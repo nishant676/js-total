6b7313ab9bb89775d4f013ac9c67fb5a35556d51
Nishant Mishra <88461593+nishant676@users.noreply.github.com>
2025-01-28 18:57:03 +0000

    Operations and action with e.g

---
+++ 01_basics/03a_conversionOperation.js
@@ -0,0 +1,21 @@
+//****************Operations****************
+let value = 3;
+let negVal = -value;
+console.log(negVal)
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+*/
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+
+console.log(true)  // true
+console.log(+true) // 1
+console.log(+"")  //0
+

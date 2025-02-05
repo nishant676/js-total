055061cf59bf5641997569e1aca50f1a84dae50f
Nishant Mishra <88461593+nishant676@users.noreply.github.com>
2025-02-05 19:32:05 +0000

    Info about json

---
+++ 02_basics/05_jsonIntro.js
@@ -0,0 +1,41 @@
+//------------------------------------Json-------------
+/*
+    API :- when ever u throw your work to someone that is called api
+          => e.g:-when u visit hotel for food , u choose food it may be samosa /dosa then u don't
+                  have to worry for its preparation.
+                    rest of the things handle by chef or someone else 
+                    API is like that just tell what to do he will do it for you.
+         => u get some value from backend how will you write  this work is done by API
+         =>  Old days the value provided  by XML format ---> complex
+NOTE:- =>    Now days   value provided by   JSON  format
+*/
+
+
+
+// => Json write inside {} --curly brace
+{
+
+
+}
+
+// here json value is like key and value pair ---> 
+// key shoul dbe in double coat
+
+//--- (i)API in object format
+{
+/*
+    "name" : "hatodi";
+    "coursename" : "ding ding ding";
+    "price" : "free";*/
+}
+
+// ---(ii) API in array format
+
+[
+    {},
+    {},
+    {}
+]
+
+// some api Name s like :- randomeAPI------------
+// To understand api tool like :-----json formatter

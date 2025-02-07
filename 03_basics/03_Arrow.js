287333170cfaef6490a2ac507e06848744d9cb5a
Nishant Mishra <88461593+nishant676@users.noreply.github.com>
2025-02-07 20:40:03 +0000

    this keyword uses & Syntax Arrow function

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,70 @@
+
+// this-->
+//      => It refer currect context.
+const box ={
+    color:"red",
+    quant : 5,
+
+    welcomeMessage:function(){
+        console.log(`${this.color}, welcome here`)
+    }
+
+}
+box.welcomeMessage() //red, welcome here
+
+box.color="BLUE";
+box.welcomeMessage()//BLUE, welcome here
+ 
+/*
+    => Here u can see value change from red to blue
+    => it happen because we used "this"
+*/
+
+console.log(this); //{}--> empty cause nothing is there
+
+
+// this will work only for obj it will not wrk for function see E.G
+function food ()
+{
+    let spice = "mirchi"
+    console.log(this.spice); //undefined
+}
+food();
+
+//--------------ARROW FUNCTION --------------------
+/*
+    Syntax:-
+        () => {}
+    --> u can hold it by a variable
+
+*/
+const lunch =  () =>
+{
+    let spice = "mirchi"
+    console.log(this.spice); //undefined
+}
+lunch();
+
+//e.g:-
+const addNum = (num1,num2) =>
+{
+    return num1 + num2
+}
+
+console.log(addNum(2,1)); //3
+
+/* Arrow function used in different way too like
+    -----IMPLICITE RETURN------
+    => here we do not use {} 
+    => one line 
+    => if Use {}  ---> return write
+       if Use ()  ---> Not req
+    e.g:
+*/
+
+const mulNum = (num1,num2) =>  (num1 * num2)
+console.log(mulNum(2,4)); //8
+
+
+
+

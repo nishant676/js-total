1df777d55aa2ebf1d65bb121157c950a9fce2ed3
Nishant Mishra <88461593+nishant676@users.noreply.github.com>
2025-02-16 18:27:46 +0000

    organise

--- 06_bg_color_project/changeColor.js
+++ 06_bg_color_project/changeColor.js
@@ -1,3 +1,5 @@
+
+
 const buttons = document.querySelectorAll('.button')
 
 const body = document.querySelector('body');
@@ -20,10 +22,10 @@ buttons.forEach(function(button)
     if(e.target.id === 'blue')
     {
      body.style.backgroundColor = e.target.id;
-            }
-            if(e.target.id === 'yellow')
-                {
-                    body.style.backgroundColor = e.target.id;
-                }
+    }
+    if(e.target.id === 'yellow')
+    {
+    body.style.backgroundColor = e.target.id;
+    }
 })
 })
